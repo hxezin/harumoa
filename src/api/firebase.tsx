e20173b0f4e947e78a53537b498aa239b9ce198c
jung_developer <59546994+wjdgml3092@users.noreply.github.com>
2023-12-03 18:08:36 +0900

    feat: firebase db 추가

--- src/api/firebase.tsx
+++ src/api/firebase.tsx
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -13,5 +14,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-
-export default app
+const db = getDatabase(app)
