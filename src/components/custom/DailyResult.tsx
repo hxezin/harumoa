0838a12af68574d3ea24cc3ef51868ee00e1307b
hxezin <leehxejin@gmail.com>
2023-12-23 23:09:19 +0900

    feat: 일간 내역 커스텀

--- src/components/custom/DailyResult.tsx
+++ src/components/custom/DailyResult.tsx
@@ -1,6 +1,35 @@
+import styled from 'styled-components'
+import { dailyResultCategory, dailyResultOptions } from '../../constants'
+import useDropdown from '../../hooks/useDropdown'
+
+const DropdownContainer = styled.div`
+  position: relative;
+  width: fit-content;
+  cursor: pointer;
+`
+
+const DropdownInput = styled.input`
+  width: 100%;
+  cursor: pointer;
+`
+
+const DropdownList = styled.ul`
+  position: absolute;
+  top: 100%;
+  left: 0%;
+  margin: 0;
+  padding: 0.2rem;
+  width: 100%;
+  background-color: #fff;
+  border: 1px solid black;
+
+  li:hover {
+    background-color: #f7f6f6;
+  }
+`
+
 interface DailyResultProps {
   dailyResult: string
-
   setDailyResult: (data: string) => void
   isEdit: boolean
 }
@@ -10,7 +39,36 @@ const DailyResult = ({
   setDailyResult,
   isEdit,
 }: DailyResultProps) => {
-  return <div>{dailyResult}</div>
+  const { isOpen, selectedValue, toggleDropdown, dropdownRef, selectOption } =
+    useDropdown({
+      defaultSelectedValue: dailyResultCategory[dailyResult] || '',
+      onSelect: (option) => {
+        setDailyResult(option)
+      },
+    })
+
+  return (
+    <section>
+      <h3>일간 내역</h3>
+      <DropdownContainer ref={dropdownRef}>
+        <DropdownInput
+          onClick={toggleDropdown}
+          value={selectedValue}
+          disabled={!isEdit}
+          readOnly
+        />
+        {isEdit && isOpen && (
+          <DropdownList>
+            {dailyResultOptions.map((item) => (
+              <li key={item} onClick={() => selectOption(item)}>
+                {dailyResultCategory[item]}
+              </li>
+            ))}
+          </DropdownList>
+        )}
+      </DropdownContainer>
+    </section>
+  )
 }
 
 export default DailyResult
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -21,3 +21,11 @@ export const paymentTypeOptions = [
   { label: '현금', value: 'cash' },
   { label: '카드', value: 'card' },
 ]
+
+export const dailyResultOptions = ['income', 'expense', 'revenue']
+
+export const dailyResultCategory: { [key: string]: string } = {
+  income: '수입',
+  expense: '지출',
+  revenue: '합계',
+}
