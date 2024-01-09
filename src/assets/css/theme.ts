95197a4d2f07a56091fdacd0299e9c87cf9ef606
jung_developer <59546994+wjdgml3092@users.noreply.github.com>
2024-01-09 14:56:55 +0900

    feat: 색깔 상수 파일 정의

---
+++ src/assets/css/theme.ts
@@ -0,0 +1,38 @@
+const fontSize = {
+  xs: '0.68rem', //11px
+  sm: '0.75rem', //12px
+  base: '1rem', //16px
+  md: '1.25', //20px
+  lg: '1.5rem', //24px
+  xl: '2.25rem', //36px
+  xxl: '2.5rem', //40px
+}
+
+const fontWeight = {
+  extraBold: '800',
+  bold: '700',
+  semiBold: '600',
+  medium: '500',
+  regular: '400',
+}
+
+const color = {
+  white: '#FAFAFA',
+  black: '#030303',
+
+  gray0: '#E4E4E4',
+  gray1: '#B4B4B4',
+  gray2: '#616161',
+  gray3: '#333333',
+
+  primary: '#85A0E6',
+  error: '#E68585',
+}
+
+const theme = {
+  fontSize,
+  fontWeight,
+  color,
+}
+
+export default theme
