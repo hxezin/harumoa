8d10de048fcb4e75b01448349c2f9b3d39c8bb95
hxezin <leehxejin@gmail.com>
2024-01-11 17:51:58 +0900

    feat: 버튼 컴포넌트

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,71 @@
+import styled, { css } from 'styled-components'
+
+// disabled style
+const disabledButtonStyle = css`
+  border: 1px solid ${({ theme }) => theme.color.gray0} !important;
+  color: ${({ theme }) => theme.color.gray0};
+  background: ${({ theme }) => theme.color.white};
+  box-shadow: 0;
+  cursor: not-allowed;
+`
+
+// 기본 버튼
+const Container = styled.button`
+  padding: 0.3rem 0.7rem;
+  border-radius: 1.43rem;
+  border: 1px solid ${({ theme }) => theme.color.gray1};
+  background: ${({ theme }) => theme.color.white};
+  box-shadow: 1px 1px 3px 0px rgba(97, 97, 97, 0.5);
+
+  color: ${({ theme }) => theme.color.gray1};
+  font-weight: ${({ theme }) => theme.fontWeight.semiBold};
+  font-size: ${({ theme }) => theme.fontSize.base};
+
+  ${(props) => props.disabled && disabledButtonStyle};
+`
+
+interface ButtonProps {
+  value: string
+  onClick: () => void
+  disabled?: boolean
+}
+
+export const Button = ({ value, onClick, disabled }: ButtonProps) => {
+  return (
+    <Container onClick={onClick} disabled={disabled}>
+      {value}
+    </Container>
+  )
+}
+
+// 파란색 버튼(e.g. 저장 버튼)
+const BlueButtonContainer = styled(Container)`
+  background-color: ${({ theme }) => theme.color.primary};
+  color: ${({ theme }) => theme.color.white};
+
+  ${(props) => props.disabled && disabledButtonStyle};
+`
+
+export const BlueButton = ({ value, onClick, disabled }: ButtonProps) => {
+  return (
+    <BlueButtonContainer onClick={onClick} disabled={disabled}>
+      {value}
+    </BlueButtonContainer>
+  )
+}
+
+// 빨간색 버튼(e.g. 삭제 버튼)
+const RedButtonContainer = styled(Container)`
+  background-color: ${({ theme }) => theme.color.error};
+  color: ${({ theme }) => theme.color.white};
+
+  ${(props) => props.disabled && disabledButtonStyle};
+`
+
+export const RedButton = ({ value, onClick, disabled }: ButtonProps) => {
+  return (
+    <RedButtonContainer onClick={onClick} disabled={disabled}>
+      {value}
+    </RedButtonContainer>
+  )
+}
