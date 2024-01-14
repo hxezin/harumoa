21fb5f520473f47828e7a9d8f6b1852093a31236
hxezin <leehxejin@gmail.com>
2024-01-14 21:27:01 +0900

    style: form 요소 디자인 변경

--- src/assets/css/form.ts
+++ src/assets/css/form.ts
@@ -5,13 +5,13 @@ export type TextAlign = 'center' | 'left' | 'right'
 export const formFieldStyles = ($textAlign: TextAlign) => css`
   box-sizing: border-box;
   width: 100%;
-  padding: 0.5rem;
+  padding: 0.75rem;
   border: 2px solid ${({ theme }) => theme.color.gray1};
   border-radius: 0.5rem;
   background-color: ${({ theme }) => theme.color.white};
 
-  color: ${({ theme }) => theme.color.black};
-  font-size: ${({ theme }) => theme.fontSize.sm};
+  color: ${({ theme }) => theme.color.gray3};
+  font-size: ${({ theme }) => theme.fontSize.base};
   font-weight: ${({ theme }) => theme.fontWeight.medium};
   text-align: ${$textAlign || 'center'};
 
@@ -41,7 +41,7 @@ export const formFieldViewModeStyles = (
       : theme.color.gray0};
     border: 2px solid
       ${$viewMode ? theme.color.secondary.main : theme.color.gray0};
-    color: ${$viewMode ? theme.color.primary.main : theme.color.gray1};
+    color: ${$viewMode ? theme.color.primary.dark : theme.color.gray1};
     font-weight: ${({ theme }) => theme.fontWeight.medium};
   }
 `
