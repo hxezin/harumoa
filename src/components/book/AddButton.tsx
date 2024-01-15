5ef8db7842d8d0531972ddef43f6c1e4b61adbfe
hxezin <leehxejin@gmail.com>
2024-01-15 19:11:08 +0900

    refactor: AccontBookTable 컴포넌트 분리

---
+++ src/components/book/AddButton.tsx
@@ -0,0 +1,34 @@
+import { GrayBorderButton } from '../common/Button'
+import { ReactComponent as Add } from '../../assets/icons/addCircle.svg'
+import { styled } from 'styled-components'
+
+const AddIcon = styled(Add)`
+  width: 1.5rem;
+  height: 1.5rem;
+
+  circle {
+    fill: ${({ theme }) => theme.color.primary.main};
+  }
+
+  path {
+    stroke: ${({ theme }) => theme.color.white};
+  }
+`
+
+interface AddButtonProps {
+  onClick: () => void
+}
+
+const AddButton = ({ onClick }: AddButtonProps) => {
+  return (
+    <GrayBorderButton
+      value={<AddIcon />}
+      onClick={onClick}
+      padding='0.25rem 0'
+      width='95%'
+      borderRadius='0.5rem'
+    />
+  )
+}
+
+export default AddButton
