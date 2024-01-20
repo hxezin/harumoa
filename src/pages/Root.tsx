a4d676fa885c4513e5b63c6ae199ad667d854fe4
hxezin <leehxejin@gmail.com>
2024-01-20 18:48:48 +0900

    feat: 로그인 중이 아닌 경우 라우팅 처리

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,8 +1,19 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { useAuthContext } from '../components/context/AuthContext'
 import Header from '../components/layout/Header'
 import RootContainer from '../components/layout/RootContainer'
 
 const Root = () => {
+  const { isLoggedIn } = useAuthContext()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate('/login')
+    }
+  }, [isLoggedIn, navigate])
+
   return (
     <RootContainer>
       <Header />
